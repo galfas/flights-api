d6c3d9d30849f4e8dac962ec4a439d1ff2b9bed1
Marcelo Juan Saciloto <marcelo.saciloto@walmart.com>
2016-06-24 23:28:59 -0300

    Insert basic auth and the the main class

--- src/main/java/com/mtt/airline/main/Application.java
+++ src/main/java/com/mtt/airline/main/Application.java
@@ -1,18 +1,46 @@
 package com.mtt.airline.main;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages={"com.mtt"})
 @PropertySource(value={"classpath:application.properties"})
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+}
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Configuration 
+class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
+	
+	@Value("${security.auth.password}")
+	private String password;
+	
+	@Value("${security.auth.user}")
+	private String user;
+	
+	@Value("${security.auth.role}")
+	private String defaultRole;
+	
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser(user).password(password).roles(defaultRole);
     }
 }
---
+++ src/test/java/com/mtt/airline/controller/ApplicationTest.java
@@ -0,0 +1,44 @@
+package com.mtt.airline.controller;
+
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+import com.mtt.airline.main.Application;
+
+
+@SuppressWarnings("deprecation")
+public class ApplicationTest {
+	
+
+	@InjectMocks
+	Application app = new Application();
+
+	
+	@Test
+	public void shouldHaveSpringBoot() {
+		SpringBootApplication annotation = this.app.getClass().getAnnotation(SpringBootApplication.class);
+
+		Assert.assertThat(annotation, is(SpringBootApplication.class));
+	}
+
+	
+	@Test
+	public void shouldHaveARootPath() {
+		EnableGlobalMethodSecurity annotation = this.app.getClass().getAnnotation(EnableGlobalMethodSecurity.class);
+		Assert.assertThat(annotation, is(EnableGlobalMethodSecurity.class));
+		Assert.assertTrue(annotation.securedEnabled());
+	}
+	
+	@Test
+	public void shouldHavePropertySource() {
+		PropertySource annotation = this.app.getClass().getAnnotation(PropertySource.class);
+		Assert.assertThat(annotation, is(PropertySource.class));
+	}
+	
+}
